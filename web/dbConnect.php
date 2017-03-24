dedb94f272722d134831bc61bdbf49935a0b28e5
Tyler Wiest <jtwiest@gmail.com>
2017-03-24 20:03:09 +0000

    Update mysql functions in connectDB

--- web/dbConnect.php
+++ web/dbConnect.php
@@ -1,16 +1,17 @@
 <?php
 function connectDB() {
-	//EDIT SERVER INFO
-	$server="localhost"; $username="inotes"; $password="inotes"; $datebase_name="content"
-    $con = mysql_connect($server,$username,$password);
-    mysql_set_charset('utf8', $con);
+    //EDIT SERVER INFO
+    $server="localhost"; $username="inotes"; $password="inotes"; $datebase_name="content";
+    $con = mysqli_connect($server,$username,$password);
+    mysqli_set_charset($con, 'utf8');
     if (!$con) {
         die('Could not connect: '.mysql_error());
     }
-    mysql_select_db($datebase_name,$con);
+    mysqli_select_db($con, $datebase_name);
+    return $con;
 }
 
-function disconnectDB() {
-    mysql_close();
+function disconnectDB($con) {
+    mysqli_close($con);
 }
 ?>
