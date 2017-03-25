493ebf99241219cd444ca0eb31a429d70ceae874
Tyler Wiest <jtwiest@gmail.com>
2017-03-25 13:46:32 +0000

    Update mysql function referneces

--- CreateContent/updateDB.php
+++ CreateContent/updateDB.php
@@ -3,7 +3,7 @@ session_start();
 include('globals.php');
 
 //Select the iNotes database
-$success = mysqli_select_db($dbname);
+$success = mysqli_select_db($link, $dbname);
 
 if ($_SESSION["photo"] == "")
 	$newAnnotation = $_SESSION["annotation"];
@@ -11,15 +11,15 @@ else
 	$newAnnotation = $_SESSION["annotation"]."$".$_SESSION["photo"].",".$_SESSION["caption"];
 
 //Sanatize the user input for the SQL query
-$newAnnotation = mysqli_escape_string($newAnnotation);
+$newAnnotation = mysqli_escape_string($link, $newAnnotation);
 $sql_query_string = ("UPDATE `" . $_SESSION["name"] . "` 
 SET `" . $_SESSION["track"] . "` = '" . $newAnnotation . "' 
 WHERE MeasureNumber = '" . $_SESSION["measure"] . "'");
 
 //Send the query with the appropriate variables	
-mysqli_query("SET NAMES 'utf8'");
-mysqli_query("SET CHARACTER SET 'utf8'");
-$response =  mysqli_query($sql_query_string);
+mysqli_query($link, "SET NAMES 'utf8'");
+mysqli_query($link, "SET CHARACTER SET 'utf8'");
+$response =  mysqli_query($link, $sql_query_string);
 
 //Let the user know whether or not the update succeded
 if ($response == false)
