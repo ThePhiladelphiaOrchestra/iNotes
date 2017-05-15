735fe009d3826be9608ce469c95032a1fcef8ef3
Kevin Clough <kevinclough@gmail.com>
2017-05-15 19:24:05 -0400

    Update configs and Vagrantfile.

--- Vagrantfile
+++ Vagrantfile
@@ -9,5 +9,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.box_version = "20160319.0.0"
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.network "private_network", ip: "172.30.1.10"
+  config.vm.network :public_network
+
+  config.vm.synced_folder "configs/", "/opt/livenote-configs"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,27 +4,40 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password roo
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password rootpass'
 
 sudo apt-get update
-sudo apt-get -y install git mysql-server-5.7 mysql-common apache2 php7.0 php7.0-mysql libapache2-mod-php php-mcrypt php7.0-gd
+sudo apt-get -y install git mysql-server-5.7 mysql-common apache2 php7.0 php7.0-mysql libapache2-mod-php php-mcrypt php7.0-gd build-essential libmysqlclient-dev
 
-if ! [ -L /var/livenote ]; then
-  rm -rf /var/livenote
-  git clone https://github.com/JarvusInnovations/iNotes.git /var/livenote
-  ln -fs /var/livenote/web /var/www/html
-  ln -fs /var/livenote/CreateContent /var/www/html
+if ! [ -L /opt/livenote ]; then
+  rm -rf /opt/livenote
+  git clone https://github.com/JarvusInnovations/iNotes.git /opt/livenote
+  cp /opt/livenote-configs/apache/livenote.conf /etc/apache2/sites-available/
+
+  a2ensite livenote
+  a2dissite 000-default
   service apache2 restart
 fi
 
 if [ ! -f /var/log/databasesetup ]; then
 	echo "CREATE USER 'inotes'@'localhost' IDENTIFIED BY 'inotes'" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
-    echo "CREATE DATABASE content" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
-    echo "GRANT ALL ON content.* TO 'inotes'@'localhost'" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
-    echo "CREATE DATABASE content_dev" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
-    echo "GRANT ALL ON content_dev.* TO 'inotes'@'localhost'" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
-    echo "flush privileges" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
+  echo "CREATE DATABASE content" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
+  echo "GRANT ALL ON content.* TO 'inotes'@'localhost'" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
+  echo "CREATE DATABASE content_dev" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
+  echo "GRANT ALL ON content_dev.* TO 'inotes'@'localhost'" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
+  echo "flush privileges" | mysql --defaults-extra-file=/etc/mysql/debian.cnf
 
-    mysql --defaults-extra-file=/etc/mysql/debian.cnf content < /var/livenote/database/CurrentConcert.sql
-    mysql --defaults-extra-file=/etc/mysql/debian.cnf content < /var/livenote/database/Strauss,\ Don\ Juan_\ Don\ Juan.sql
-    mysql --defaults-extra-file=/etc/mysql/debian.cnf content < /var/livenote/database/currentMeasure.sql
+  mysql --defaults-extra-file=/etc/mysql/debian.cnf content < /opt/livenote/database/CurrentConcert.sql
+  mysql --defaults-extra-file=/etc/mysql/debian.cnf content < /opt/livenote/database/Strauss,\ Don\ Juan_\ Don\ Juan.sql
+  mysql --defaults-extra-file=/etc/mysql/debian.cnf content < /opt/livenote/database/currentMeasure.sql
 
-    touch /var/log/databasesetup
+  touch /var/log/databasesetup
+fi
+
+if [ ! -f /var/log/multicastsetup ]; then
+  cd /opt/livenote/multicast-server/transmission
+  cc -D _BSD_SOURCE -o multicast-server MulticastServerPush.c `mysql_config --cflags --libs`
+
+  cp /opt/livenote-configs/systemd/multicast-server.service /etc/systemd/system/multicast-server.service
+  systemctl enable multicast-server
+  service multicast-server start
+  
+  touch /var/log/multicastsetup
 fi
